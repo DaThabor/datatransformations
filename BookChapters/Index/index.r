cb6652f7cc163383d84f519558674a87bd560741
Thabor Walbeek <37037843+DaThabor@users.noreply.github.com>
2021-03-27 05:22:27 +0530

    Added book chapters

---
+++ BookChapters/Index/index.r
@@ -0,0 +1,32 @@
+--- 
+title: "Data Transformations Principles"
+subtitle: "Comprehensive guide to data transformations principles"
+author: "Thabor Walbeek"
+date: "`r Sys.Date()`"
+site: bookdown::bookdown_site
+documentclass: book
+bibliography:
+- book.bib
+- packages.bib
+biblio-style: apalike
+link-citations: yes
+description: A book to describe the basic principles of working with data transformations.
+output:
+  bookdown::gitbook:
+    split_by: "section"
+    split_bib: no
+---
+
+# Data Principles
+
+![](images/Data principles.png){width=40%}
+
+
+Isn't there a day going by without you searching something on Google? 
+
+This book wit data principles will help you find your information quickly. "Quicker than Google", you might ask? Well, not entirely, but as everything you need in your day-to-day life is combined into this book in an easy read, you might actually be faster than finding things on Google...
+
+
+'Data Principals' is a book for all data enthusiasts out there. In our daily lives we are working with data in many forms and ways, and if you are a Data Scientist, Data Engineer, BI consultant, Data Analyst, or any other profession working with data, you will always face some kind of problems when dealing with data.
+
+This book will help you identify the basic principles of data in many ways. Each chapter deals with a particular part of data and explains the basic principles. With those principles and some guidance here and there, you should be able to find a solution for your particular problem and maybe learn something new along the way!
---
+++ BookChapters/Intro/intro.r
@@ -0,0 +1,10 @@
+# Introduction {#intro}
+
+My name is Thabor Walbeek, Data Scientist from profession, but working in all areas of data: from solution architect to BI professional. 
+As one of the few non-technical background guys, I have had to learn my fair share before I was able to work with data in many different ways. 
+Data as such is a broad concept and has many faces. I hope this book will help gathering all information I normally have to search for and spend many hours to do so. 
+In this book you might (quickly) find a topic of your interest and I hope it will help you in getting things done much faster.
+
+If you have any queries, please contact me at: thabor@dathabor.com
+
+![DATHABOR](DATHABOR_logo.jpg)
