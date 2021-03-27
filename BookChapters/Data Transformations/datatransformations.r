98d316f1b91c0abc1d66115dc129d69d598d3413
Thabor Walbeek <37037843+DaThabor@users.noreply.github.com>
2021-03-27 05:53:03 +0530

    Added chapter data transformations content

---
+++ BookChapters/Data Transformations/datatransformations.r
@@ -0,0 +1,16 @@
+# Data Transformations {#datatrans}
+
+# Introduction
+
+This is an Introduction
+
+# Types of Data Transformations
+
+In this book we divide the transformations over the following categories:
+
+1. Data Type transformations
+2. Filtering Data
+3. Select Columns
+4. Remove Columns and Rows
+5. Derive new information
+
